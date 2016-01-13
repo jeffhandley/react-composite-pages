ddb302b36adf3769c16d87e0484a48f89a76803c
Jeff Handley <jeff.handley@concur.com>
2016-01-13 11:38:44 -0800

    Extract a redux loadClient module

--- src/pages/redux/client.js
+++ src/pages/redux/client.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Section from '../../masters/components/Section';
 import Title from '../../masters/components/Title';
-import { createStore, bindActionCreators } from 'redux';
-import { Provider } from 'react-redux';
 import Message from './components/Message';
+import { Provider } from 'react-redux';
 import reducers from './reducers';
+import actionCreators from './actionCreators';
+import loadClient from '../../redux/loadClient';
 
-const state = window.redux;
-const container = document.getElementById('pages-redux');
-
-const store = createStore(reducers, state);
-
-const Component = () => (
-    <Section name='body' id='pages-redux'>
-        <div>
-            <Title title='Redux Page' />
-            <Provider {...{store}}>
-                <Message />
-            </Provider>
-        </div>
-    </Section>
-);
-
-ReactDOM.render(<Component />, container);
+loadClient({
+    reducers,
+    actionCreators,
+    component: (props) => (
+        <Section name='body' id='pages-redux'>
+            <div>
+                <Title title='Redux Page (on the client)' />
+                <Provider store={props.store}>
+                    <Message />
+                </Provider>
+            </div>
+        </Section>
+    ),
+    state: window.redux,
+    container: document.getElementById('pages-redux')
+}, ({setSource}) => {
+    setSource('client');
+});
